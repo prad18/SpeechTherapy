5ee5d225477c99b3bbba2c578d519dc1676d7096
pradish <pradishvaidyas@gmail.com>
2024-03-12 20:53:28 +0530

    Update base views and URLs

---
+++ speechtherapy/base/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('letters/', views.letters, name='letters'),
+    path('base/',views.base, name='base'),
+]
